ed4b4901ad3091da13c7437cf626739418ecb72e
nenad seke <nenad seke@localhost>
2011-10-13 23:36:10 +0200

    Added seesow for creating gui in application.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject leinProjekat "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [seesaw "1.2.0"]]
   :main leinProjekat.core)
